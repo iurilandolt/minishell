32db8c6a82366ef4cb6094192613abc439aa0282
iuri~ <rlandolt@student.42lisboa.com>
2024-02-21 14:55:23 +0000

    path check for cmd and cmd paths

---
+++ sources/pipeline/check_bin_path.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_bin_path.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 14:50:16 by rlandolt          #+#    #+#             */
+/*   Updated: 2024/02/21 14:51:33 by rlandolt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/token.h"
+#include "../../include/read.h"
+
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	while (i < n)
+	{
+		*((char *)dest + i) = *((const char *)src + i);
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*dest;
+	size_t	size;
+
+	size = ft_strlen(str) + 1;
+	dest = (char *)malloc(sizeof(char) * size);
+	if (!dest)
+		return (NULL);
+	ft_memcpy(dest, str, size);
+	return (dest);
+}
+
+char	*check_bin_path(char **envp, char *cmd)
+{
+	int		i;
+	char	**paths; // could be storeed on instance struct?
+	char	*path;
+	char	*program;
+
+	i = 0;
+	if (access(cmd, F_OK) == 0)
+		return (ft_strdup(cmd)); // strdup wont be needed when we use execve?
+	while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) == 0)
+		i++;
+	if (!envp[i])
+		return (NULL);
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (*(paths + i))
+	{
+		path = ft_strjoin(paths[i], "/"); // 63 64 65; could be handle by an auxiliar function?
+		program = ft_strjoin(path, cmd);
+		free(path);
+		if (access(program, F_OK | X_OK) == 0)
+		{
+			clear(paths);
+			return (program);
+		}
+		free(program);
+		i++;
+	}
+	return (clear(paths));
+}
