398c522df07fbe9004c53336c1d0a8425c5e96ff
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-03-20 22:57:13 +0000

    splited m_export() into aux function for norm compliance

--- sources/Tasks/Builtins/env/menvp.c
+++ sources/Tasks/Builtins/env/menvp.c
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 12:12:11 by rlandolt          #+#    #+#             */
-/*   Updated: 2024/03/20 22:24:56 by rlandolt         ###   ########.fr       */
+/*   Updated: 2024/03/20 22:40:29 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,32 +59,35 @@ void	print_export(char **menvp)
 	clear(dupped);
 }
 
-void	m_export(int *status, char ***menvp, char *value)
+static void	export_value(char ***menvp, char *value)
 {
 	char	**parsed;
 
+	parsed = parse_for_export(value);
+	if (menvp_lookup(parsed[0], *menvp) != -1)
+	{
+		if (!parsed[1] || parsed[1][0] == '=')
+			export_operation(menvp, value, parsed, 0);
+		else if (parsed[1][0] == '+')
+			concat_export(menvp, parsed);
+	}
+	else if (parsed[2])
+		export_operation(menvp, NULL, parsed, 1);
+	else if (parsed[1] && !parsed[2])
+		export_operation(menvp, NULL, parsed, 2);
+	else
+		*menvp = export_to_menvp(parsed[0], *menvp);
+	return (free_table(parsed));
+}
+
+void	m_export(int *status, char ***menvp, char *value)
+{
 	if (!*menvp)
 		return ;
 	if (!value)
 		return (print_export(*menvp));
 	if (is_valid_env_format(value))
-	{
-		parsed = parse_for_export(value);
-		if (menvp_lookup(parsed[0], *menvp) != -1)
-		{
-			if (!parsed[1] || parsed[1][0] == '=')
-				export_operation(menvp, value, parsed, 0);
-			else if (parsed[1][0] == '+')
-				concat_export(menvp, parsed);
-		}
-		else if (parsed[2])
-			export_operation(menvp, NULL, parsed, 1);
-		else if (parsed[1] && !parsed[2])
-			export_operation(menvp, NULL, parsed, 2);
-		else
-			*menvp = export_to_menvp(parsed[0], *menvp);
-		return (free_table(parsed));
-	}
+		return (export_value(menvp, value));
 	if (!exp_is_protected(value))
 		m_export_error(status, value);
 }
